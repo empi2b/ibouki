a1f850f81bf0f06930e82a61d242588b88bb83c0
emmanuel <empi2b@gmail.com>
2014-05-21 19:23:05 -0400

    ....

--- src/Calldirek/UserBundle/Services/LoginSuccessHandler.php
+++ src/Calldirek/UserBundle/Services/LoginSuccessHandler.php
@@ -29,7 +29,6 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
     public function onAuthenticationSuccess(Request $request, TokenInterface $token)
     {
         $response = null;
-
         if($this->security->isGranted('ROLE_SUPER_ADMIN')){
 
             $response = new RedirectResponse($this->router->generate('fos_user_profile_show'));
@@ -46,7 +45,7 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
             if($referrerUrl){
                 $response = new RedirectResponse($referrerUrl);
             } else {
-                $response = new RedirectResponse($this->router->generate('fos_user_profile'));
+                $response = new RedirectResponse($this->router->generate('fos_user_profile_show'));
             }
         }
 
