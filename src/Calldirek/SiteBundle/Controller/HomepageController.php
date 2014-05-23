f9aa34b332c0d533c1e0d62e5f00d261c553e2e7
emmanuel <empi2b@gmail.com>
2014-05-22 22:08:51 -0400

    configured guzzle rest client service

--- src/Calldirek/SiteBundle/Controller/HomepageController.php
+++ src/Calldirek/SiteBundle/Controller/HomepageController.php
@@ -2,18 +2,37 @@
 
 namespace Calldirek\SiteBundle\Controller;
 
+use GuzzleHttp\Exception\RequestException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomepageController extends Controller
 {
     public function indexAction()
     {
-        /** @var \Guzzle\Http\Client $precashClient */
-        $precashClient = $this->container->get('guzzle.precash.client');
+        $response = null;
+        $data = null;
 
-        $status = $precashClient->get('statuses/user_timeline.json');
+        /** @var \GuzzleHttp\Client $precashFinancialClient */
+        $precashFinancialClient = $this->container->get('financial.services');
 
-        var_dump($status); exit;
-        return $this->render('SiteBundle:Homepage:index.html.twig');
+        try{
+
+            $response = $precashFinancialClient->get('/user');
+
+        }catch (RequestException $e){
+
+            echo 'RequestException : '.$e->getRequest().'<br />';
+            echo 'Message : '.$e->getMessage().'<br />';
+            if($e->hasResponse()){
+                echo 'Exception 2 : '.$e->getResponse();
+            }
+
+        }
+
+        echo 'Status = '.$response->getStatusCode().'<br />';
+        echo 'Reason Phrase = '.$response->getReasonPhrase().'<br />';
+        echo 'Effective Url = '.$response->getEffectiveUrl().'<br />';
+
+        return $this->render('SiteBundle:Homepage:index.html.twig', array('response' => $response));
     }
 } 
